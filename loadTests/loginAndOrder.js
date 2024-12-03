a58b9ca978f4b6bbccff224d476bb7bcab4ba8cc
Hyrum-Taylor <82358970+Hyrum-Taylor@users.noreply.github.com>
2024-12-02 17:39:26 -0700

    add(K6 tests) saving working script for K6 testing here

---
+++ loadTests/loginAndOrder.js
@@ -0,0 +1,303 @@
+import { sleep, check, group, fail } from 'k6'
+import http from 'k6/http'
+
+export const options = {
+  cloud: {
+    distribution: { 'amazon:us:ashburn': { loadZone: 'amazon:us:ashburn', percent: 100 } },
+    apm: [],
+  },
+  thresholds: {},
+  scenarios: {
+    Scenario_1: {
+      executor: 'ramping-vus',
+      gracefulStop: '30s',
+      stages: [
+        { target: 5, duration: '30s' },
+        { target: 15, duration: '1m' },
+        { target: 10, duration: '30s' },
+        { target: 0, duration: '30s' },
+      ],
+      gracefulRampDown: '30s',
+      exec: 'scenario_1',
+    },
+  },
+}
+
+export function scenario_1() {
+  let response
+
+  group('page_1 - https://pizza.climb-5.com/', function () {
+    // load page
+    response = http.get('https://pizza.climb-5.com/', {
+      headers: {
+        accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'cache-control': 'no-cache',
+        priority: 'u=0, i',
+        'sec-ch-ua': '"Microsoft Edge";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+      },
+    })
+    sleep(3)
+
+    // login
+    response = http.put(
+      'https://pizza-service.climb-5.com/api/auth',
+      '{"email":"user1@user.com","password":"user1"}',
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          'cache-control': 'no-cache',
+          'content-type': 'application/json',
+          origin: 'https://pizza.climb-5.com',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Microsoft Edge";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-site',
+        },
+      }
+    )
+    if (!check(response, { 'status equals 200': response => response.status.toString() === '200' })) {
+      console.log("login: ", response.body);
+      fail('Login was *not* 200');
+    }
+
+    const verify_token_list = {};
+    verify_token_list[`token${__ITER}-${__VU}`] = JSON.parse(response.body).token;
+
+    response = http.options('https://pizza-service.climb-5.com/api/auth', null, {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'access-control-request-headers': 'content-type',
+        'access-control-request-method': 'PUT',
+        'cache-control': 'no-cache',
+        origin: 'https://pizza.climb-5.com',
+        priority: 'u=1, i',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+    sleep(3.9)
+
+    // get menu
+    response = http.get('https://pizza-service.climb-5.com/api/order/menu', {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'cache-control': 'no-cache',
+        'content-type': 'application/json',
+        origin: 'https://pizza.climb-5.com',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Microsoft Edge";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+
+    response = http.options('https://pizza-service.climb-5.com/api/order/menu', null, {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'access-control-request-headers': 'authorization,content-type',
+        'access-control-request-method': 'GET',
+        'cache-control': 'no-cache',
+        origin: 'https://pizza.climb-5.com',
+        priority: 'u=1, i',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+
+    // get franchises
+    response = http.get('https://pizza-service.climb-5.com/api/franchise', {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'cache-control': 'no-cache',
+        'content-type': 'application/json',
+        origin: 'https://pizza.climb-5.com',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Microsoft Edge";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+
+    response = http.options('https://pizza-service.climb-5.com/api/franchise', null, {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'access-control-request-headers': 'authorization,content-type',
+        'access-control-request-method': 'GET',
+        'cache-control': 'no-cache',
+        origin: 'https://pizza.climb-5.com',
+        priority: 'u=1, i',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+    sleep(7.1)
+
+    // order pizza
+    response = http.post(
+      'https://pizza-service.climb-5.com/api/order',
+      '{"items":[{"menuId":1,"description":"Veggie","price":0.0038}],"storeId":"1","franchiseId":1}',
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          'cache-control': 'no-cache',
+          'content-type': 'application/json',
+          origin: 'https://pizza.climb-5.com',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Microsoft Edge";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-site',
+          'authorization': `Bearer ${verify_token_list['token'+__ITER+'-'+__VU]}`,
+        },
+      }
+    )
+    if (!check(response, { 'status equals 200': response => response.status.toString() === '200' })) {
+      console.log("order pizza: ", response.body);
+      //fail('Login was *not* 200');
+    }
+
+    const jwt_list = {};
+    jwt_list[`token${__ITER}-${__VU}`] = JSON.parse(response.body).jwt;
+
+    response = http.options('https://pizza-service.climb-5.com/api/order', null, {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'access-control-request-headers': 'authorization,content-type',
+        'access-control-request-method': 'POST',
+        'cache-control': 'no-cache',
+        origin: 'https://pizza.climb-5.com',
+        priority: 'u=1, i',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+    sleep(2.3)
+
+    // verify order
+    response = http.post(
+      'https://pizza-factory.cs329.click/api/order/verify',
+      `{"jwt": "${jwt_list['token'+__ITER+'-'+__VU]}"}`,
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          'cache-control': 'no-cache',
+          'content-type': 'application/json',
+          origin: 'https://pizza.climb-5.com',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Microsoft Edge";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'cross-site',
+          'authorization': `Bearer ${verify_token_list['token'+__ITER+'-'+__VU]}`,
+        },
+      }
+    )
+    if (!check(response, { 'status equals 200': response => response.status.toString() === '200' })) {
+      console.log("verify order: ", response.body);
+      //fail('Login was *not* 200');
+    }
+
+    response = http.options('https://pizza-factory.cs329.click/api/order/verify', null, {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'access-control-request-headers': 'authorization,content-type',
+        'access-control-request-method': 'POST',
+        'cache-control': 'no-cache',
+        origin: 'https://pizza.climb-5.com',
+        priority: 'u=1, i',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'cross-site',
+      },
+    })
+    sleep(4.8)
+
+    // logout
+    response = http.del('https://pizza-service.climb-5.com/api/auth', null, {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'cache-control': 'no-cache',
+        'content-type': 'application/json',
+        origin: 'https://pizza.climb-5.com',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Microsoft Edge";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+        'authorization': `Bearer ${verify_token_list['token'+__ITER+'-'+__VU]}`,
+      },
+    })
+    if (!check(response, { 'status equals 200': response => response.status.toString() === '200' })) {
+      console.log("logout: ", response.body);
+      //fail('Login was *not* 200');
+    }
+
+    response = http.options('https://pizza-service.climb-5.com/api/auth', null, {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'access-control-request-headers': 'authorization,content-type',
+        'access-control-request-method': 'DELETE',
+        'cache-control': 'no-cache',
+        origin: 'https://pizza.climb-5.com',
+        priority: 'u=1, i',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+        'authorization': `Bearer ${verify_token_list['token'+__ITER+'-'+__VU]}`,
+      },
+    })
+  })
+}
